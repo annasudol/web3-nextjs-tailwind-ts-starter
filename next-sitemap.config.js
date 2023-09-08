03ba45c4c0563b3fa5b7e85ccc631588950b976e
Anna Sudol <sudolann@gmail.com>
2023-09-08 17:36:12 +0200

    Add Url

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -2,7 +2,7 @@
  * @type {import('next-sitemap').IConfig}
  */
 module.exports = {
-  siteUrl: '',
+  siteUrl: 'https://web3-nextjs-tailwind-ts-starter.vercel.app/',
   generateRobotsTxt: true,
   robotsTxtOptions: {
     policies: [{ userAgent: '*', allow: '/' }],
